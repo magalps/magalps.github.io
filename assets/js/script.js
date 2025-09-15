7261c1a374ee4267bb34c25ae1303854cae63296
magalps <and.soares@live.com>
2025-09-15 14:16:33 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -242,7 +242,7 @@ for (let i = 0; i < navigationLinks.length; i++) {
     const data = await res.json();
 
     // Education
-    const eduList = findTimelineListByHeading('Educação');
+    const eduList = findTimelineListByHeading('Education');
     if (eduList && Array.isArray(data.education)) {
       eduList.innerHTML = data.education.map(ed => `
         <li class="timeline-item">
@@ -257,7 +257,7 @@ for (let i = 0; i < navigationLinks.length; i++) {
     }
 
     // Experience
-    const expList = findTimelineListByHeading('Experiência');
+    const expList = findTimelineListByHeading('Experience');
     if (expList && Array.isArray(data.experience)) {
       expList.innerHTML = data.experience.map(ex => `
         <li class="timeline-item">
